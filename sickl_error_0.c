549594225d8126df4651c8a127fd4ca4a4fd5828
Ayoub Amzouar <aamzouar@e1r7p4.1337.ma>
2021-01-17 19:24:09 +0100

    sesco: fuck you sickl_

--- sickl_error_0.c
+++ sickl_error_0.c
@@ -38,9 +38,9 @@ void	e_builtin(void)
 	EPRINTS(": ");
 	if (g_builtin_errno != EB_CD_HNT && g_builtin_errno != EB_CD_EXIT_TMA)
 	{
-		// EPRINTS("`");
+		g_bash_commandid == BC_UNSET ? EPRINTS("`") : 0;
 		EPRINT(g_bash_error);
-		// EPRINTS("'");
+		g_bash_commandid == BC_UNSET ? EPRINTS("'") : 0;
 		EPRINTS(": ");
 	}
 	if (g_builtin_errno != -1)
